b97ef12fad1aed5d7cb8b66a9649cfbd5e3202d8
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-06-30 00:03:51 +0400

    call methods

--- js.js
+++ js.js
@@ -142,3 +142,37 @@ user.display = function(){
  
 // მეთოდის გამოძახება
 user.display();
+///////////////
+var user = {
+    name: "Tom",
+    age: 26,
+    display: function () {
+        console.log(this.name);
+        console.log(this.age);
+    }
+};
+// მეთოდის გამოძახება
+user.display();
+////////////
+var user = {
+    "name": "Tom",
+    "age": 26,
+    "display": function () {
+        console.log(user.name);
+        console.log(user.age);
+    }
+};
+// მეთოდის გამოძახება
+user.display();
+///////////
+var user = {
+    name: "Tom",
+    age: 26,
+    "full name": "Tom Johns",
+    "display info": function () {
+        console.log(user.name);
+        console.log(user.age);
+    }
+};
+console.log(user["full name"]);
+user["display info"]();
