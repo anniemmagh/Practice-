f62d64ee0db84d0ce496cb63e8254348f0ef4b36
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-07-22 23:33:36 +0400

    Update js.js

--- js.js
+++ js.js
@@ -459,3 +459,24 @@ console.log(user.age);
 
 // მეთოდის გამოძახება
 user.display();
+////////////////
+function User(pName, pAge) {
+    this.name = pName;
+    this.age = pAge;
+    this.displayInfo = function(){
+    document.write("სახელი: " + this.name + "; ასაკი: " + this.age + "<br/>");
+    };
+    };
+    
+    User.prototype.hello = function(){
+    document.write(this.name + " ამბობს: 'გამარჯობა!'<br/>");
+    };
+    User.prototype.maxAge = 110;
+    
+    var tom = new User("ტომი", 26);
+    tom.hello();
+    var john = new User("ჯონი", 28);
+    john.hello();
+    console.log(tom.maxAge); // 110
+    console.log(john.maxAge); // 110
+    
